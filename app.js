383198cc88e6107f052f8c9070e0e04f7371c009
uSilky <39656396+uSilky@users.noreply.github.com>
2018-06-29 19:36:02 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,43 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+const config = require("./config.json");
+
+client.on("ready", () => {
+
+  console.log(`BOT ON!`); 
+
+});
+
+client.login(config.token);
+
+client.on("message", async message => {
+
+    if(message.author.bot) return;
+    
+    if(message.content.indexOf(config.prefix) !== 0) return;
+
+    const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+    const command = args.shift().toLowerCase();
+
+    if(command === "anunciar") {
+        if (message.member.hasPermission('MANAGE_GUILD')) {  
+               
+            let mensg = args.join(" ");
+            if(!mensg)
+               return message.channel.send("Digite uma mensagem de aviso!")
+               
+            const anuncio = new Discord.RichEmbed()
+               .setColor("#555D50")
+               .setAuthor("Anúncio", "https://cdn.discordapp.com/emojis/460264772869554176.gif")
+               
+               .setDescription(mensg)
+               
+               .setTimestamp()
+               .setFooter(`Por: ${message.author.tag}`, message.author.avatarURL)
+            
+            message.channel.send("@everyone", anuncio)
+    
+    }
+}
+});
